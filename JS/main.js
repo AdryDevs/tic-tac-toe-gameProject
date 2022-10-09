614cc329c4e1ce40d9f0a369129bdb3665d29e56
AdryDevs <alonsoadrian86@gmail.com>
2022-10-09 19:50:41 +0200

    diseño terminado, botones añadidos. Comienzo parte JS

---
+++ JS/main.js
@@ -0,0 +1,57 @@
+// Board
+
+let board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+    //Draw
+
+const cell1 = document.getElementById('c1');
+const txtCell1 = document.getElementById('p1');
+
+function DrawCircle (txtCell1) {
+    txtCell1.innerText = "O";
+}
+
+
+
+// Players
+
+let player1;
+
+let player2;
+
+// New Game
+
+const StartNewGame = (board) => board = board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+// Turns
+
+// CPU (randomize moves)
+
+//Reset
+
+
+// Draw circle with transition?????
+
+
+    // circle.enter().append("circle")
+    //     .attr("class", "dot");
+
+    // circle
+    //     .attr("r", 5)
+    //     .attr("cy", 20)
+    //     .attr("cx", function(d,i){return i*50;})
+    //     .attr("fill", "red");
+
+    // svg.selectAll("circle")
+    //     .style("opacity", 0)
+    //     .transition()
+    //     .duration(1000)
+    //     .style("opacity", 1);
