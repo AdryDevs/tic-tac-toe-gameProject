0c91547ad9e778e440da607eb6251942ce42c70a
AdryDevs <alonsoadrian86@gmail.com>
2022-10-12 13:38:36 +0200

    minor adds and structures ready on JS

--- JS/main.js
+++ JS/main.js
@@ -6,30 +6,95 @@ let board = [
     [0, 0, 0]
 ];
 
-    //Draw
+    //Counting
 
-const cell1 = document.getElementById('c1');
-const txtCell1 = document.getElementById('p1');
+for (let i = 0; i < board.length; i++){
 
-function DrawCircle (txtCell1) {
-    txtCell1.innerText = "O";
 }
 
+    //Winning moves
+
+if((board[0][0]=="X") && (board[1][0]=="X") && (board[2][0]=="X")) {
+    alert("The winner is ", );
+}
+
+    //Draw in cell
+
+let cellsCollection = document.getElementsByClassName("cell");
+
+let cells = Array.from(cellsCollection);
+
+let switching = true;
+
+cells.map((cell)=>{
+    cell.addEventListener('click',()=>{
+        if(cell.innerHTML == ""){
+            cell.innerHTML = (switching) ? "X" : "O";
+            switching = !switching;
+        };
+    });
+});
+
+
+// Check if cell is active or not to draw
+
+// si la posición es = 0 ("") entonces pinto círculo o X, si no es ="" no pinto
 
 
 // Players
 
-let player1;
+let player1 = {
+    player: document.getElementById("selectorPlayer1").value, //CPU o Player?
+    name: document.getElementById("player1"),  //Player's name
+    selector: document.getElementById("signPlayer1")   // X or O?
+}
 
-let player2;
+
+let player2 = {
+    player: document.getElementById("selectorPlayer2").value, 
+    name: document.getElementById("player2"),  
+    selector: document.getElementById("signPlayer2")   
+}
+
+if(document.getElementById("slectorPlayer1").value == "CPU"){
+    player1.name = "CPU";
+}
+
+if(document.getElementById("slectorPlayer2").value == "CPU"){
+    player2.name = "CPU";
+}
+
+    //Converting and saving players' data in sesionStorage
+
+let player1Conv = JSON.stringify(player1);
+let player2Conv = JSON.stringify(player2);
+
+sessionStorage.setItem("Player1Data",player1Conv);
+sessionStorage.setItem("Player2Data",player2Conv);
+
+    //Reconverting and getting the players' data from sesionStorage
+
+let storageDataPlayer1 = localStorage.getItem("Player1Data");
+let storageDataPlayer2 = localStorage.getItem("Player2Data");
+
+let player1Reconv = JSON.parse(storageDataPlayer1);
+let player2Reconv = JSON.parse(storageDataPlayer2);
+
+//Player's Display
+
+let displayPlayer1 = document.getElementById("Player1Display");
+
+displayPlayer1.innerHTML = player1.name;
 
 // New Game
 
-const StartNewGame = (board) => board = board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
+let start = document.getElementById("start");
+
+start = false;
+
+if(start = false){
+    
+};
 
 // Turns
 
